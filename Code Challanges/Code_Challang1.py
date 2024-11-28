ab39cac231a07f21d12f604bcd861652dee73137
gb8462 <planetes8462@gmail.com>
2024-11-28 20:31:18 +0800

    New Update

---
+++ Code Challanges/Code_Challang1.py
@@ -0,0 +1,3 @@
+# Prints A Diamond At The Center Of The Terminal
+# -- using only one line of code --
+print("\n\t\t\t\t\t\t\t\t\t\t* \n\t\t\t\t\t\t\t\t\t\t\b\b* * * \n\t\t\t\t\t\t\t\t\t\t\b\b\b\b* * * * * \n" + "\t\t\t\t\t\t\t\t\t\t\b\b\b\b\b\b\b\b  * * * * * * * \n\t\t\t\t\t\t\t\t\t\t\b\b\b\b* * * * * \n\t\t\t\t\t\t\t\t\t\t\b\b* * * \n\t\t\t\t\t\t\t\t\t\t*")
