d380882dd245268c290704d85096730922be10d2
gb8462 <planetes8462@gmail.com>
2024-12-06 02:31:34 +0800

    New Feature added

--- Code_Activities/Activity1.py
+++ Code_Activities/Activity1.py
@@ -1,2 +1,2 @@
 def act1():
-    print("Typing Test: Forgot My Result")
+    print("\nTyping Test: \nWords per minute: 63\nAccuracy: 94%\nTime: 60 seconds\nConsistency: 75%")
--- Code_Challanges/Code_Challange13.py
+++ Code_Challanges/Code_Challange13.py
@@ -2,17 +2,17 @@ def Code13():
     for r in range(1, 8):
         for i in range(8, r, -1):
             print(" ", end="")
-        for j in range(r - 1, 0, -1):
+        for j in range(r - 1, 1, -1):
             print(j, end="")
-        for k in range(0, r):
+        for k in range(1, r):
             print(k, end="")
         print()
 
-    for t in range(6, 0, -1):
+    for t in range(6, 1, -1):
         for x in range(8, t, -1):
             print(" ", end="")
-        for y in range(t - 1, 0, -1):
+        for y in range(t - 1, 1, -1):
             print(y, end="")
-        for z in range(0, t):
+        for z in range(1, t):
             print(z, end="")
         print()
