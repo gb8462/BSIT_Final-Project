84f3d69ac8829e488ce6d8d404da8d39ecbcc14e
gb8462 <planetes8462@gmail.com>
2024-12-10 06:50:52 +0800

    New Feature

--- Code_Challenges/Code_Challenge14.py
+++ Code_Challenges/Code_Challenge14.py
@@ -1,2 +1,24 @@
+import os
+
+# This Program is made by Terence-tarrega
+# Here is a link to his GitHub Profile! Check him out sometime :D
+# https://github.com/terence-tarrega
+
 def Code14():
-    print("\nCurrenly Unavailable\n")
+    tuloy = True
+    x = 0
+
+    print("Enter a number. \nIf you put a 0 in it, then the progarm will terminate and you will get the sum of all the number/s you typed in:")
+
+    while tuloy == True:
+        number = eval(input("Enter a number--->  "))
+        if number == 0:
+            os.system('cls')
+            print(f"The total of the number/s you entered is : [{x}]")
+            print()
+            print("::::Program Terminated, Thank You!::::")
+            print()
+            break
+        else:
+            x += number
+            continue
