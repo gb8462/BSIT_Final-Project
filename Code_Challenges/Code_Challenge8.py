6800f8c2c8c5a438b8e1b75a89c15b6082b979a2
gb8462 <planetes8462@gmail.com>
2024-12-10 06:44:39 +0800

    New Feature

--- Code_Challenges/Code_Challenge8.py
+++ Code_Challenges/Code_Challenge8.py
@@ -1,2 +1,17 @@
 def Code8():
-    print("\nCurrenly Unavailable\n")
+    odd_count = 0
+    even_count = 0
+    total_sum = 0
+
+    for i in range(1, 11):
+        user_input = int(input(f"Enter number {i}: "))
+        total_sum += user_input
+        
+        if user_input % 2 == 0:
+            even_count += 2
+        else:
+            odd_count += 1
+
+    print(f"\nThe total sum of the numbers you entered is: {total_sum}")
+    print(f"Even numbers tally: {even_count}")
+    print(f"Odd numbers tally: {odd_count}")
