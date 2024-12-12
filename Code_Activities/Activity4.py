57116058555bf4dedde9a4886ab20c81f94ca460
gb8462 <planetes8462@gmail.com>
2024-12-13 05:38:55 +0800

    Activity 4 and 5 added

--- Code_Activities/Activity4.py
+++ Code_Activities/Activity4.py
@@ -1,2 +1,17 @@
 def act4():
-    print("Currently in progress")
+	print("========================================\n")
+	num1 = eval(input("Number: "))
+	num2 = eval(input("Number: "))
+	op = input("Operators(+, -, /, x): ")
+
+	if op == "+":
+		print("Output:", num1 + num2)
+	elif op == "-":
+		print("Output:", num1 - num2)
+	elif op == "/":
+			print("Output:", num1 / num2)
+	elif op == "x":
+		print("Output:", num1 * num2)
+	else:
+		print("Invalid operator! \n\n")
+	print("\n========================================")
--- Code_Activities/Activity5.py
+++ Code_Activities/Activity5.py
@@ -1,2 +1,5 @@
 def act5():
-    print("Currently in progress")
+    temp = int(input("Enter temperature in Fahrenheit: "))
+    cel = (temp - 32) * 5 / 9
+    cel_r = round(cel, 2)
+    print(f"\nThe conversion of  {temp} degrees Fahrenhet is {cel_r} degrees celsius.\n")
