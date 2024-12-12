dc09fc3a45859442dd6cdba19c87f754ab2d16ce
gb8462 <planetes8462@gmail.com>
2024-12-13 05:30:37 +0800

    activity 2

--- Code_Activities/Activity3.py
+++ Code_Activities/Activity3.py
@@ -1,2 +1,7 @@
 def act3():
-    print("Currently in progress")
+    name = input("What is your name: ")
+    age = input("How old are you: ")
+    email = input("What is your email: ")
+    birthday = input("When is your Birthday: ")
+    add = input("Enter your Address: ")
+    print(f"\nHi my name is {name} and I am {age} yrs old. I was born in {birthday} And I am from {add}. My email is {email}")
