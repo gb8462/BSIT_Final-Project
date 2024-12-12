3de8e02f97b26801f68a34a37ddc93326b564992
gb8462 <planetes8462@gmail.com>
2024-12-13 05:40:48 +0800

    Activity 6

--- Code_Activities/Activity6.py
+++ Code_Activities/Activity6.py
@@ -1,2 +1,11 @@
 def act6():
-    print("Currently in progress")
+    x = 5
+    print(f"{x}")
+    x = x + 10
+    print(x)
+    x = x + 15
+    print(x)
+    x = x + 20
+    print(x)
+    x = x + 25
+    print(f"{x}\n")
