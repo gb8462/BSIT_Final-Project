ee9d9ec9c4333309f089fbb07690afb47487a7ec
gb8462 <planetes8462@gmail.com>
2024-12-13 05:58:27 +0800

    Activity 16

--- Code_Activities/Activity16.py
+++ Code_Activities/Activity16.py
@@ -1,2 +1,12 @@
 def act16():
-    print("Currently in progress")
+    for x in range(1, 6):
+        for y in range(1, x + 1):
+            print("  ",end="")
+        for z in range(6, y, -1):
+            print("^ ",end="")
+        for a in range(6, x, -1):
+            print("* ",end="")
+        for b in range(1, x + 1):
+            print(" ",end="")
+        print("\n")
+    print()
