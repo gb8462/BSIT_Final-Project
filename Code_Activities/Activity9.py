2894168a98ef1bd574ef89296fab6f0b34086328
gb8462 <planetes8462@gmail.com>
2024-12-13 05:46:38 +0800

    Activity 8

--- Code_Activities/Activity9.py
+++ Code_Activities/Activity9.py
@@ -1,2 +1,18 @@
 def act9():
-    print("Currently in progress")
+    age = int(input("Enter your age: "))
+    if age <= 1:
+        print("You are a Toddler\n")
+    elif age <= 8:
+        print("You are a Pre-teen\n")
+    elif age <= 14: 
+        print("You are Teenager\n")
+    elif age <= 19:
+        print("You are in Early adulthood\n")
+    elif age <= 32:
+        print("You are in Mid adulthood\n")
+    elif age <= 46:
+        print("You are in Post adulthood\n")
+    elif age >= 60:
+        print("You are in your Senior\n")
+    else:
+        print("Invalid syntax\n")
