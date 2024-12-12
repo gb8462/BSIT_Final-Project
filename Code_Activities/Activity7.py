f94f0358418987768232603b1e3c2c207c244371
gb8462 <planetes8462@gmail.com>
2024-12-13 05:44:03 +0800

    Activity 7

--- Code_Activities/Activity7.py
+++ Code_Activities/Activity7.py
@@ -1,2 +1,11 @@
 def act7():
-    print("Currently in progress")
+    miner = input("Enter you name: ")
+    mined = input("Did you mine gold today (yes/no) : ")
+    gold = 0
+    if mined.lower() == "yes":
+        gold += 1
+        print(f" Your total mined today is {gold} gold\n")
+    elif mined.lower() == "no":
+        print(f" You havent mined a gold today\n")
+    else:
+        print("Invalid error\n")
