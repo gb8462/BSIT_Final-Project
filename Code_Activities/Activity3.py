6b1401904c80569b943517d89b8126065fce8393
gb8462 <planetes8462@gmail.com>
2024-12-13 05:31:19 +0800

    activity 3

--- Code_Activities/Activity3.py
+++ Code_Activities/Activity3.py
@@ -3,5 +3,5 @@ def act3():
     age = input("How old are you: ")
     email = input("What is your email: ")
     birthday = input("When is your Birthday: ")
-    add = input("Enter your Address: ")
-    print(f"\nHi my name is {name} and I am {age} yrs old. I was born in {birthday} And I am from {add}. My email is {email}")
+    address = input("Enter your Address: ")
+    print(f"\nHi my name is {name} and I am {age} yrs old. I was born in {birthday} And I am from {address}. My email is {email}")
