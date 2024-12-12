f12cf594d9d4662188df424a295c7a72ee9ebc7f
gb8462 <planetes8462@gmail.com>
2024-12-13 06:12:18 +0800

    Activity 19 and 20

--- Code_Activities/Activity19.py
+++ Code_Activities/Activity19.py
@@ -1,2 +1,12 @@
 def act19():
-    print("Currently in progress")
+    while True:
+        name = input("Are You Happy? (yes/ no): ")
+        if name.lower() == "yes":
+            print("I See Good For you!")
+            break
+        elif name.lower() == "no":
+            print("Okay Comeback when your happy\n")
+            break
+        else:
+            print("Invalid input exiting")
+            break
--- Code_Activities/Activity20.py
+++ Code_Activities/Activity20.py
@@ -1,2 +1,20 @@
+# Code Made By Arglen... I think
 def act20():
-    print("Currently in progress")
+    condition = True
+    num = 0
+    while condition == True:
+        question = str(input("Do you want to add triangles (yes/no): "))
+        if question.lower() == "no":
+            print("PROGRAM TERMINATED\n")
+            break
+        else:
+            num += 1
+            for a in range(1,5):
+                for x in range(1, num+1 ):
+                    for y in range(1, a + 1):
+                            print("*", end= " ")
+                            
+                    for z in range(5, a, -1):
+                            print(" ", end=" ")
+                print()
+        continue
