3b966b7f88eb5b2ec50ac03316f5c30ad11a1e7f
gb8462 <planetes8462@gmail.com>
2024-12-13 06:01:43 +0800

    Activity 17 and 18

--- Code_Activities/Activity17.py
+++ Code_Activities/Activity17.py
@@ -1,6 +1,6 @@
 def act17():
-   elu = int(input("Enter a number: "))
+   ragud = int(input("Enter a number: "))
    for x in range(1,11):
-        for y in range(1, elu + 1):
+        for y in range(1, ragud + 1):
             print(f"{x} x {y} = {x*y}", end="\t")
         print()
--- Code_Activities/Activity18.py
+++ Code_Activities/Activity18.py
@@ -1,2 +1,10 @@
 def act18():
-    print("Currently in progress")
+    ragud = int(input("Enter a number: "))
+    for a in range(1,5):
+        for x in range(1,ragud +1 ):
+            for y in range(1, a + 1):
+                print("*", end= " ")
+
+            for z in range(5, a, -1):
+                print(" ", end=" ")
+        print()
