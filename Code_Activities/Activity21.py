50662413142372c44ef387d4b84b83e5d363f3c4
gb8462 <planetes8462@gmail.com>
2024-12-16 05:51:07 +0800

    Activity 21

--- Code_Activities/Activity21.py
+++ Code_Activities/Activity21.py
@@ -1,2 +1,16 @@
 def act21():
-    print("Currently Unavailable")
+    tuloy = True
+    nameno = 0
+    while tuloy == True:
+        name = input("Enter a name: ")
+        
+
+        if name.lower()=="stop":
+            print("Okay tama na\n")
+            print(f"You have entered a total of {nameno} names!")
+            break
+
+        else:
+            print("type 'stop' if you want to terminate the program\n")
+            nameno += 1
+            continue
